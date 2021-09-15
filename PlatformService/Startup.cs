51ebd2fad6b9c9fce9ca909f248af6c5c30a712b
Pierre BIZARD <bizard.pierre@hotmail.fr>
2021-09-15 12:13:30 +0200

    Stop à 1:33:20

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -1,19 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Net.Security;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PlatformService.Data;
 
 namespace PlatformService
 {
@@ -69,6 +70,8 @@ namespace PlatformService
                 {
                     endpoints.MapControllers();
                 });
+
+            PrepDb.PrepPopulation(app);
         }
     }
 }
